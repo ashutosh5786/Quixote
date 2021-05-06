982389a64decfcde86c32efde0e5c4cf6e249b67
Ashutosh <ashutosh5786@outlook.com>
2021-05-06 19:41:15 +0530

    Created the bcket

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const app = express();
 const AWS = require("aws-sdk");
 var uuid = require("uuid");
 
+// Set the region
+AWS.config.update({ region: "ap-south-1" });
+// const { s3 } = require("aws-sdk");
+
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
 // Credentials for AWS account
 var credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
 AWS.config.credentials = credentials;
 
 // Create unique bucket name
+// const bucketName = uuid.v4();
+const params = {
+  Bucket: "38e227e6-4956-480f-8bc7-655a34ca0e28",
+  ACL: "public-read",
+  CreateBucketConfiguration: {
+    LocationConstraint: "ap-south-1",
+  },
+};
+try {
+  s3.createBucket(params, function (err, data) {
+    console.log("Succefully Created" + data);
+  });
+} catch (err) {}
 
-app.listen(80, () => {
-  console.log("Listening on http://localhost:80");
-});
+app.listen(80);
