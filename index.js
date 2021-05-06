bac2dd893d889f213b7a8287f1c7d294d090bd71
Ashutosh <ashutosh5786@outlook.com>
2021-05-06 21:28:15 +0530

    Succefully Mange to upload file and handle err

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const AWS = require("aws-sdk");
 var uuid = require("uuid");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
 
 // Set the region
 AWS.config.update({ region: "ap-south-1" });
@@ -16,17 +23,56 @@ AWS.config.credentials = credentials;
 
 // Create unique bucket name
 // const bucketName = uuid.v4();
-const params = {
-  Bucket: "38e227e6-4956-480f-8bc7-655a34ca0e28",
-  ACL: "public-read",
-  CreateBucketConfiguration: {
-    LocationConstraint: "ap-south-1",
-  },
-};
+
 try {
-  s3.createBucket(params, function (err, data) {
-    console.log("Succefully Created" + data);
-  });
+  s3.createBucket(
+    {
+      //params
+      Bucket: "38e227e6-4956-480f-8bc7-655a34ca0e28",
+      ACL: "public-read",
+      CreateBucketConfiguration: {
+        LocationConstraint: "ap-south-1",
+      },
+    },
+    function (err, data) {
+      console.log("Succefully Created" + data);
+    }
+  );
 } catch (err) {}
 
+try {
+  var upload = multer({
+    limits: { fileSize: 125000 },
+    storage: multerS3({
+      s3: s3,
+      bucket: "38e227e6-4956-480f-8bc7-655a34ca0e28",
+      key: function (req, file, cb) {
+        // Set the filetypes
+        var filetypes = /jpeg|jpg|png/;
+        var mimetype = filetypes.test(file.mimetype);
+        var extname = filetypes.test(
+          path.extname(file.originalname).toLowerCase()
+        );
+
+        if (mimetype && extname) {
+          return cb(null, file.originalname);
+        }
+        cb(null, false);
+      },
+    }),
+  }).array("photos", 10);
+} catch (err) {
+  res.send("Not Upload");
+}
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/uploadProfilePicture", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) res.send("File must be photo(png/jpg/jpeg) and less than 1MB");
+    else res.send("Uploaded");
+  });
+});
 app.listen(80);
